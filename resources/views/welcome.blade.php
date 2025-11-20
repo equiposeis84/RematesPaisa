a86043ca964d6d0cf6de6cff1d31bb02a7140ec3
sebbxs999 <sebbxsbt911@gmail.com>
2025-11-20 13:28:44 -0500

    Actualizacion de iconos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Remates El Paísa - Administración @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
@@ -22,9 +22,9 @@
                     <li class="sidebar-fixed"><a href="#" data-view="html/Catalogo.html"> <span class="nav-icon"> <i class="fa-solid fa-house"></i> </span>Inicio </a></li>
                     <li class="has-submenu sidebar-fixed">
                         <button class="submenu-toggle" aria-expanded="false" aria-controls="gestion-submenu">
-                            <span class="nav-icon"> <i class="fa-solid fa-gear"></i> </span>Gestión <span class="caret"> <i class="fa-solid fa-caret-down"></i> </span>
+                            <span class="nav-icon"> <i class="fa-solid fa-gear"></i> </span>Gestión <span class="caret"> <i class="fa-solid fa-circle-chevron-down"></i> </span>
                         </button>
-                        <ul id="gestion-submenu" class="submenu" hidden>
+                        <ul id="gestion-submenu" class="submenu" show>
                             <li><a href="{{route('clientes.index')}}" data-view=""> <span class="nav-icon"> <i class="fa-solid fa-users"></i> </span>Clientes </a></li>
                             <li><a href="{{route('productos.index')}}" data-view=""> <span class="nav-icon"> <i class="fa-solid fa-box-archive"></i> </span> Productos </a></li>
                             <li><a href="{{route('pedidos.index')}}" data-view=""> <span class="nav-icon"> <i class="fa-solid fa-bag-shopping"></i> </span>Pedidos </a></li>
