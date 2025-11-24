4d8830566dea3ae0e308a679dd7b266aa697eda3
sebbxs999 <200297973+sebbxs999@users.noreply.github.com>
2025-11-24 12:06:19 -0500

    Intento1deIntegracion

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
+   ->withMiddleware(function (Middleware $middleware) {
+    $middleware->alias([
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'role' => \App\Http\Middleware\CheckRole::class,
+    ]);
+})
+
+   
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
