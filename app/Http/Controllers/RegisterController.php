8a1ec28f7043c973051a2c3a4535a467a6ad09c1
sebbxs999 <231731195+Nicolas3115679@users.noreply.github.com>
2025-11-27 13:54:34 -0500

    ..

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -44,10 +44,11 @@ class RegisterController extends Controller
 
         // 2. Guardar en tabla USUARIOS
         Usuarios::create([
-            'nombreUsuario' => $*-.request->nombreUsuario,
+            'nombreUsuario' => $*-.request->nombreUsuario,  
             'passwordUsuario' => Hash::make($request->passwordUsuario),
             //'idRoles' => $request->idRoles // Usuarios por defecto (id = 2)
             'idRoles' => 2 // Usuarios por defecto (id = 2) 
+            'idCliente' => $cliente->idCliente
 
         ]);
 
